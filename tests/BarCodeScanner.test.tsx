731658bffb72d0427909a27e2b767549e8db0ada
adrian <mendezadrian149@gmail.com>
2020-10-08 20:15:02 -0400

    adding other test

--- tests/BarCodeScanner.test.tsx
+++ tests/BarCodeScanner.test.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import BarCodeScanner from '../src/index';
 
 describe('<BarCodeScanner />', () => {
-    const wrapper = shallow(<BarCodeScanner onUpdate={(err, result) => {}}/>);
-  
+    const callback = jest.fn();
+    const wrapper = mount(<BarCodeScanner onUpdate={callback} />);
+
     test('should render correctly', () => {
-       expect(wrapper).toMatchSnapshot();
+        const wrapper = shallow(<BarCodeScanner onUpdate={callback} />);
+        expect(wrapper).toMatchSnapshot();
     });
 
 
     test('should match the properties', () => {
-       const props =  wrapper.find('video').props();
-       expect(props.id).toBe('video');
-       expect(props.className).toBe('dbrScanner-video');
-       expect(props.playsInline).toBe(true);
+        const props = wrapper.find('video').props();
+        expect(props.id).toBe('video');
+        expect(props.className).toBe('dbrScanner-video');
+        expect(props.playsInline).toBe(true);
+        expect(callback).toHaveBeenCalled();
+        expect(wrapper).toMatchSnapshot();
+        wrapper.unmount();
     });
 })
